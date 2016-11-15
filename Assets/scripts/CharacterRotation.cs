d9eb4340c46d4b98a0849196ab6935bd552d3a0e
emiven <emiven@hotmail.no>
2016-11-15 23:26:05 +0100

    helvete

--- Assets/scripts/CharacterRotation.cs
+++ Assets/scripts/CharacterRotation.cs
@@ -7,8 +7,10 @@ public class CharacterRotation : MonoBehaviour {
 
 	private Vector3 mousePos;
 	private Vector3 pivotPos;
+	public Vector3 fa;
+	public float deadzone = 0.25f;
+	public float satan;
 	public float angle;
-
 	public static bool JOYSTICK = true;
 	public static bool MOUSE = false;
 
@@ -16,33 +18,43 @@ public class CharacterRotation : MonoBehaviour {
 	void Start () {
 		camTarget = FindObjectOfType<CameraTargetController> ();
 	}	
+
+	void rotasjon()
+	{
+		float joyVertical = Input.GetAxis ("rightJoystickVertical");
+		float joyHorizontal = Input.GetAxis ("rightJoystickHorizontal");
+
+
+		satan = Mathf.Atan2 (-joyVertical, joyHorizontal);
+		angle = satan * (180/Mathf.PI); 
+
+
+
+		fa = new Vector3( 0, 0,angle );
+		if(fa.magnitude < deadzone)
+		{
+			fa = Vector3.zero;
+		}
+	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		rotasjon();
+		transform.eulerAngles = fa;
 
-		if (camTarget.inputType == MOUSE) 
-		{
-			//Mouse
-			mousePos = Input.mousePosition;
-			mousePos.z = 5f;
+//		if (camTarget.inputType == MOUSE) {
+//			//Mouse
+//			mousePos = Input.mousePosition;
+//			mousePos.z = 5f;
+//
+//			pivotPos = Camera.main.WorldToScreenPoint (transform.position);
+//			mousePos.x -= pivotPos.x;
+//			mousePos.y -= pivotPos.y;
+//
+//			angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+//		}
 
-			pivotPos = Camera.main.WorldToScreenPoint (transform.position);
-			mousePos.x -= pivotPos.x;
-			mousePos.y -= pivotPos.y;
-
-			angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-
-		} else {
-
-			//Controller
-			float joyVertical = Input.GetAxis ("rightJoystickVertical");
-			float joyHorizontal = Input.GetAxis ("rightJoystickHorizontal");
-
-			angle = Mathf.Atan2 (-joyVertical, joyHorizontal) * Mathf.Rad2Deg;
-
-		}
-
-		transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
 
 	}
 }
